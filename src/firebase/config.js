11996434a5c76c55c1ee733ffdcfb3fc1a2de86d
Ahmed Rehan <ar4084480@gmail.com>
2025-08-31 13:35:20 +0500

    franchise-minihackathone-mod-b

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAC9n3-ZJjQ8thK4ypNHlZYqo0VBJ0L9Tk",
-  authDomain: "franchise-fp.firebaseapp.com",
-  databaseURL: "https://franchise-fp-default-rtdb.firebaseio.com",
-  projectId: "franchise-fp",
-  storageBucket: "franchise-fp.firebasestorage.app",
-  messagingSenderId: "118573363628",
-  appId: "1:118573363628:web:ae29b4ae91d8aa4fbb0a09"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
